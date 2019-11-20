879412f5b4f5dab4ab805b9907772b0c2b9d0629
ViviZa <youwillnevergetthis10>
2019-11-20 12:37:30 +0100

    accept two clients

--- src/Player.java
+++ src/Player.java
@@ -6,14 +6,41 @@ public class Player implements Runnable {
     private Socket socket;
     private int id;
 
-    public Player(Socket socket, int id){
+    public Player(Socket socket, int id) {
         this.socket = socket;
         this.id = id;
     }
 
     @Override
     public void run() {
+        //receive message from client
+        while (true) {
+            InputStream is = null;
+            try {
+                is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String receivedMessage = br.readLine();
+                System.out.println("Message received from client is " + receivedMessage);
+
+                String sendMessage = "ok" + "\n";
+
+                if (receivedMessage.equals("down")) {
+                    sendMessage = "not ok" + "\n";
+                }
+                //Send response back to the client
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write(sendMessage);
+                System.out.println("Message sent to the client is " + sendMessage);
+                bw.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     public Socket getSocket() {
         return socket;
--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
@@ -9,40 +11,20 @@ public class Server {
     private List<Player> players;
 
 
-    public Server(int port){
+    public Server(int port) {
         players = new ArrayList<>();
 
         try {
             server = new ServerSocket(port);
-            Player player = new Player(server.accept(), 1);
-            while(true) {
-
-            //receive message from client
-            InputStream is = player.getSocket().getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String receivedMessage = br.readLine();
-            System.out.println("Message received from client is " + receivedMessage);
-
-            String sendMessage = "ok" + "\n";
-
-            if (receivedMessage.equals("down")) {
-                sendMessage = "not ok" + "\n";
+            ExecutorService executorService = Executors.newFixedThreadPool(200);
+            while (true) {
+                executorService.execute(new Player(server.accept(), 1));
+                executorService.execute(new Player(server.accept(), 2));
             }
 
-            //Send response back to the client
-            OutputStream os = player.getSocket().getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
-            bw.write(sendMessage);
-            System.out.println("Message sent to the client is " + sendMessage);
-            bw.flush();
-
-            players.add(player);
-            System.out.println("connected");
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
