8e688f635e3775472455aac8fa6f60c0b41745f9
Lina <lina.kriebel@me.com>
2019-12-08 17:23:45 +0100

    refactor movement processing

--- src/World.java
+++ src/World.java
@@ -3,128 +3,87 @@ public class World {
     private final int width = 10;
     private final int height = 10;
 
-    private GameItem[][] gameField ; //in gameField coordinates
+    private GameItem[][] gameField ; //in world grid coordinates
 
     public World() {
         gameField = new GameItem[width][height];
+
         GameItem player1 = new GameItem(ItemType.PLAYER, 1);
         GameItem player2 = new GameItem(ItemType.PLAYER, 2);
+
         GameItem ball = new GameItem(ItemType.BALL, 1);
-        gameField[2][2] = player1;
-        gameField[6][6] = player2;
+
+        //set up the level
+        gameField[2][2] = player1; //starting position for player 1
+        gameField[6][6] = player2; //starting position for player 2
         gameField[7][8] = ball;
     }
 
 
     public Coordinate processMove(int playerId, String direction) {
         GameItem player = getGameItem(ItemType.PLAYER, playerId);
+        Direction dir = Direction.valueOf(direction);
 
-        Coordinate position = getPosition(ItemType.PLAYER, playerId);
-        Coordinate potentialBallPosition = null;
-        Coordinate old = position;
-        Coordinate newPos = old;
-        ItemType type;
+        Coordinate currentPosition = getPosition(ItemType.PLAYER, playerId); //the position the player is currently on
+        Coordinate newPosition = getNextField(currentPosition, dir); //the position the player will move to
 
-        switch (direction) {
-            case "left":
-                if (position.x == 0) return old;
-
-                position.x -= 1;
-                if (position.x -1 != 0) {
-                    // ball is am rand
-                    potentialBallPosition = new Coordinate(position.x -1, position.y);
-                }
-
-                if (gameField[position.x][position.y] != null) {
-                    type = gameField[position.x][position.y].getType();
-                    newPos = approveMovement(position, old, type, potentialBallPosition);
-                    updateGameField(old, newPos, player);
-
-                } else {
-                    updateGameField(old, position, player);
-                }
-                break;
-            case "right":
-                if (position.x == width-1) return old;
-                position.x += 1;
-                if (position.x + 1 != width-1) {
-                    // ball is am rand
-                    potentialBallPosition = new Coordinate(position.x +1, position.y);
-                }
-
-                if (gameField[position.x][position.y] != null) {
-                    type = gameField[position.x][position.y].getType();
-                    newPos = approveMovement(position,old, type, potentialBallPosition);
-                    updateGameField(old, newPos, player);
-
-                } else {
-                    updateGameField(old, position, player);
-                }
-                break;
-            case "up":
-                if (position.y == height-1) return old;
-                position.y -= 1;
-                if (position.y != height-1) { potentialBallPosition = new Coordinate(position.x, position.y - 1); }
-
-                if (gameField[position.x][position.y] != null) {
-                    type = gameField[position.x][position.y].getType();
-                    newPos = approveMovement(position, old, type, potentialBallPosition);
-                    updateGameField(old, newPos, player);
-
-                } else {
-                    updateGameField(old, position, player);
-                }
-                break;
-            case "down":
-                if (position.y == 0) return old;
-
-                position.y += 1;
-                if (position.y -1 != 0) {
-                    // ball is am rand
-                    potentialBallPosition = new Coordinate(position.x , position.y + 1);
-                }
-
-                if (gameField[position.x][position.y] != null) {
-                    type = gameField[position.x][position.y].getType();
-                    newPos = approveMovement(position, old, type, potentialBallPosition);
-                    updateGameField(old, newPos, player);
-
-                } else {
-                    updateGameField(old, position, player);
-                }
-                break;
-        }
-
-        return newPos;
-
+        return move(currentPosition, newPosition, dir, player);
+        //TODO register player event
     }
 
-    private Coordinate approveMovement(Coordinate position, Coordinate oldPosition, ItemType type, Coordinate potentialBallPosition) {
-        Coordinate toReturn= null;
+    private Coordinate move(Coordinate currentPlayerPosition, Coordinate newPlayerPosition, Direction direction, GameItem player){
+        Coordinate playerPosition = null;
 
-        if (type.equals(ItemType.PLAYER)) {
-            // cant move
-            toReturn = oldPosition;
-        }
-        if (type.equals(ItemType.BALL)) {
-            // can ball be moved or not -> can player be moved or not
-            if (potentialBallPosition == null) {
-                toReturn = oldPosition;
-            }else {
-                GameItem ball = gameField[position.x][position.y];
-                if(gameField[potentialBallPosition.x][potentialBallPosition.y] != null) {
-                    toReturn = position;
-                    //TODO send new ball position
-                    updateGameField(position, potentialBallPosition, ball);
+        if(newPlayerPosition != null) {
+            //next field exists
+
+            if (gameField[newPlayerPosition.x][newPlayerPosition.y] != null) {
+
+                //there is something next to the player in the movement direction
+                GameItem itemInPlayerMovementDirection = gameField[newPlayerPosition.x][newPlayerPosition.y];
+                //find out what
+                ItemType type = itemInPlayerMovementDirection.getType();
+
+                if (type.equals(ItemType.PLAYER)) {
+                    //field is occupied by another player, no movement possible
+                    playerPosition = currentPlayerPosition;
                 }
+
+                if (type.equals(ItemType.BALL)) {
+                    Coordinate potentialBallPosition = getNextField(newPlayerPosition, direction);
+
+                    //check if the ball would be moved out of the gameField
+                    if (potentialBallPosition != null) {
+
+                        //ball can be moved, if the field is not occupied
+                        if (gameField[potentialBallPosition.x][potentialBallPosition.y] != null) {
+                            //field is occupied, player and ball can not be moved
+                            playerPosition = currentPlayerPosition;
+                        } else {
+                            //field is free, move ball and player to their new positions
+                            GameItem ball = gameField[newPlayerPosition.x][newPlayerPosition.y];
+                            playerPosition = newPlayerPosition;
+                            //TODO register ball event
+                            updateGameField(newPlayerPosition, potentialBallPosition, ball);
+                        }
+                    }
+                }
+
+                if (type.equals(ItemType.OBSTACLE)) {
+                    //field is an obstacle, no movement possible
+                    playerPosition = currentPlayerPosition;
+                }
+            } else {
+                //nothing in the way, the player can move to the next field
+                playerPosition = newPlayerPosition;
             }
-        }
-        if (type.equals(ItemType.OBSTACLE)) {
-            //cant move
-            toReturn = oldPosition;
+        } else {
+            //player stands at the border and can not move
+            playerPosition = currentPlayerPosition;
         }
 
-        return toReturn;
+        updateGameField(currentPlayerPosition, playerPosition, player);
+        return playerPosition;
     }
 
     private void updateGameField(Coordinate oldPos, Coordinate newPos, GameItem object) {
@@ -149,12 +108,38 @@ public class World {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 if (gameField[x][y] != null) {
-                    if (gameField[x][y].getType() == type && gameField[x][y].getId() == id) {
-                        return gameField[x][y];
+
+                    GameItem item = gameField[x][y];
+                    if (item.getType() == type && item.getId() == id) {
+                        return item;
                     }
                 }
             }
         }
+
+        //the item is not in the game (anymore)
         return null;
+    }
+
+    private Coordinate getNextField(Coordinate position, Direction direction){
+        Coordinate nextField = null;
+
+        switch(direction){
+            case LEFT:
+                if (position.x > 0) nextField = new Coordinate(position.x -1, position.y);
+                break;
+            case RIGHT:
+                if (position.x < width-1) nextField = new Coordinate(position.x +1, position.y);
+                break;
+            case UP:
+                if (position.y > height-1) nextField = new Coordinate(position.x, position.y -1);
+                break;
+            case DOWN:
+                if (position.y > 0) nextField = new Coordinate(position.x, position.y +1);
+                break;
+        }
+        //return null, if the next field would be outside the gameField
+        return nextField;
+
     }
 }
